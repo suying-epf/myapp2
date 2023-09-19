d025e04df84046c1f8ef322c3ca2b32d20523337
ying <ying.su@epfedu.fr>
2023-09-19 18:54:26 +0200

    the final version of TP1

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
+'''
 @app.route("/")
 def hello_world():
     prefix_google = """
@@ -18,3 +19,45 @@ def hello_world():
     
 """
     return prefix_google + "Hello World"
+'''
+
+
+@app.route("/")
+def root():
+    return """ 
+   <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Hello from Space  🚀 !</title>
+            
+           <!-- Google tag (gtag.js) -->
+            <script async src="https://www.googletagmanager.com/gtag/js?id=G-MJVVD9G9FC"></script>
+            <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-MJVVD9G9FC');
+            </script>
+        </head>
+        <body>
+            <h1>Hello from Space! 🚀</h1>
+            <button id="analyticsButton">Click me to send an event</button>
+
+            <script>
+                document.getElementById("analyticsButton").addEventListener("click", function () {
+                    gtag('event', 'button_click', {
+                        'event_category': 'Button',
+                        'event_label': 'Button Clicked'
+                    });
+                });
+            </script>
+        </body>
+        </html>
+    """
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
