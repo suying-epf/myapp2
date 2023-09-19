279d20429d35cd55788246fd48519fb0e6c53831
ying <ying.su@epfedu.fr>
2023-09-19 16:20:03 +0200

    third commit

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-from flask import Flask
+def hello_world():
+    prefix_google = """
+<!-- Google tag (gtag.js) -->
+<script async 
+src="https://www.googletagmanager.com/gtag/js?id=G-MJVVD9G9FC"></script> 
+<script>
+    window.dataLayer = window.dataLayer || []; 
+    function gtag(){dataLayer.push(arguments);} 
+    gtag('js', new Date());
+    gtag('config', ' YOUR_GA_CODE');
+    </script>
+"""
+    return prefix_google + "Hello World"
+    app = Flask(__name__)
 
-app = Flask(__name__)
-
-
-@app.route("/")
-def root():
-    return "Hello from Space! 🚀"
+    @app.route("/")
+    def root():
+        return "Hello from Space! 🚀"
