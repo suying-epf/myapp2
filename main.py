56f7011eee09fcd7e9fee16b1550f86eef0c2be1
ying <ying.su@epfedu.fr>
2023-09-19 16:29:49 +0200

    4th commit

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
 def hello_world():
     prefix_google = """
-<!-- Google tag (gtag.js) -->
-<script async 
-src="https://www.googletagmanager.com/gtag/js?id=G-MJVVD9G9FC"></script> 
-<script>
-    window.dataLayer = window.dataLayer || []; 
-    function gtag(){dataLayer.push(arguments);} 
-    gtag('js', new Date());
-    gtag('config', ' YOUR_GA_CODE');
+    <!-- Google tag (gtag.js) -->
+    <script async 
+    src="https://www.googletagmanager.com/gtag/js?id=G-MJVVD9G9FC"></script> 
+    <script>
+        window.dataLayer = window.dataLayer || []; 
+        function gtag(){dataLayer.push(arguments);} 
+        gtag('js', new Date());
+        gtag('config', ' G-MJVVD9G9FC');
     </script>
+    
 """
     return prefix_google + "Hello World"
-    app = Flask(__name__)
-
-    @app.route("/")
-    def root():
-        return "Hello from Space! 🚀"
