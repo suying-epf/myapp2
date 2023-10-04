18975de5613c4f6c4740fe2f0987158d23e82f7c
ying <ying.su@epfedu.fr>
2023-10-04 15:19:55 +0200

    TP2 first change

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
-from flask import Flask
+from flask import Flask, request, render_template, make_response, jsonify
+import logging
 
 app = Flask(__name__)
 
 
-'''
-@app.route("/")
-def hello_world():
-    prefix_google = """
-    <!-- Google tag (gtag.js) -->
-    <script async 
-    src="https://www.googletagmanager.com/gtag/js?id=G-MJVVD9G9FC"></script> 
-    <script>
-        window.dataLayer = window.dataLayer || []; 
-        function gtag(){dataLayer.push(arguments);} 
-        gtag('js', new Date());
-        gtag('config', ' G-MJVVD9G9FC');
-    </script>
+
+
+
+# @app.route("/")
+# def hello_world():
+#     prefix_google = """
+#     <!-- Google tag (gtag.js) -->
+#     <script async 
+#     src="https://www.googletagmanager.com/gtag/js?id=G-MJVVD9G9FC"></script> 
+#     <script>
+#         window.dataLayer = window.dataLayer || []; 
+#         function gtag(){dataLayer.push(arguments);} 
+#         gtag('js', new Date());
+#         gtag('config', ' G-MJVVD9G9FC');
+#     </script>
     
-"""
-    return prefix_google + "Hello World"
-'''
+# """
+#     return prefix_google + "Hello World"
+
 
 
 @app.route("/")
@@ -58,6 +61,27 @@ def root():
         </html>
     """
 
+@app.route("/logger")
+
+def logger():
+
+    # Log on the server side (Python)
+
+    logging.info("This is a server-side log.")
+    logging.warning('Ying SU')
+
+    # Log on the browser side
+
+    log_script = """
+
+    <script>
+
+      console.log("This is a browser-side log.");
+
+    </script>
+
+    """
+    return "Logger Page" + log_script
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=8080)
