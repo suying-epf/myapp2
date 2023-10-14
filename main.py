1c694d5fa19ada836267cdaee26702d7ab8c5cb7
ying <ying.su@epfedu.fr>
2023-10-14 17:33:37 +0200

    Tp2 finished

--- main.py
+++ main.py
@@ -1,30 +1,19 @@
 from flask import Flask, request, render_template, make_response, jsonify
-import logging
+import logging,requests
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+from google.analytics.data_v1beta import BetaAnalyticsDataClient
+from google.oauth2 import service_account
+from google.analytics.data_v1beta.types import (
+    DateRange,
+    Dimension,
+    Metric,
+    RunReportRequest,
+)
+
+
 
 app = Flask(__name__)
-
-
-
-
-
-# @app.route("/")
-# def hello_world():
-#     prefix_google = """
-#     <!-- Google tag (gtag.js) -->
-#     <script async 
-#     src="https://www.googletagmanager.com/gtag/js?id=G-MJVVD9G9FC"></script> 
-#     <script>
-#         window.dataLayer = window.dataLayer || []; 
-#         function gtag(){dataLayer.push(arguments);} 
-#         gtag('js', new Date());
-#         gtag('config', ' G-MJVVD9G9FC');
-#     </script>
-    
-# """
-#     return prefix_google + "Hello World"
-
-
-
 @app.route("/")
 def root():
     return """ 
@@ -48,7 +37,11 @@ def root():
         <body>
             <h1>Hello from Space! 🚀</h1>
             <button id="analyticsButton">Click me to send an event</button>
+                <form action="/logger" method="GET">
 
+        <button type="submit">Go to logger </button>
+    
+    </form>
             <script>
                 document.getElementById("analyticsButton").addEventListener("click", function () {
                     gtag('event', 'button_click', {
@@ -61,8 +54,7 @@ def root():
         </html>
     """
 
-@app.route("/logger")
-
+@app.route("/logger", methods=["GET"])
 def logger():
 
     # Log on the server side (Python)
@@ -81,7 +73,69 @@ def logger():
     </script>
 
     """
-    return "Logger Page" + log_script
+    return render_template('textbox.html',user_message = "je suis Ying")
+
+@app.route("/make_google_request", methods=["GET"])
+def make_google_request():
+    try:
+        # Make a request to Google
+        response = requests.get("https://www.google.com/")
+
+        # Check if the request was successful
+        if response.status_code == 200:
+            return response.cookies.get_dict()
+        else:
+            return "Google request failed with status code: " + str(response.status_code)
+
+    except Exception as e:
+        return "An error occurred: " + str(e)
+    
+@app.route("/make_google_analytics_request", methods=["GET"])
+def make_google_analytics_request():
+    try:
+        # Make a request to Google
+        response = requests.get("https://analytics.google.com/analytics/web/#/p407449646/reports/intelligenthome")
+
+        # Check if the request was successful
+        if response.status_code == 200:
+            return response.cookies.get_dict()
+        else:
+            return "Google Analytics request failed with status code: " + str(response.status_code)
+
+    except Exception as e:
+        return "An error occurred: " + str(e)
+
+################################################################
+    
+# Load Google Analytics API credentials from a JSON key file
+credentials = service_account.Credentials.from_service_account_file(
+    'ga4-project-402014-54f684c5ca17.json',
+    scopes=['https://www.googleapis.com/auth/analytics.readonly']
+)
+analytics = build('analyticsreporting', 'v4', credentials=credentials)
+
+@app.route("/get_google_analytics_data", methods=["GET"])
+def get_google_analytics_data():
+    # Fetch data from Google Analytics
+    response = analytics.reports().batchGet(
+        body={
+            'reportRequests': [
+                {
+                    'viewId': '407449646',
+                    'dateRanges': [{'startDate': '7daysAgo', 'endDate': 'today'}],
+                    'metrics': [{'expression': 'ga:users'}]
+                }
+            ]
+        }
+    ).execute()
+
+    # Extract and display the number of users (visitors)
+    data = response['reports'][0]['data']
+    user_count = data['totals'][0]['values'][0]
+
+    return f"Number of Visitors: {user_count}"
+
+
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8080)
+    app.run(host='0.0.0.0', port=8080)
